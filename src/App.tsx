67177078311b9c0f2605a62da1ca59bf9b90981f
Stephane Boulanger <stephboul22@gmail.com>
2023-01-22 18:45:47 -0500

    use ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, {id:Date.now(), todo:todo, isDone:false}]);
+      setTodo("");
+    }    
+  }
+
+  console.log(todos)
+
   return (
     <div className="App">
      <span className="heading">Taskify</span>
-     <InputFeild />
+     <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+     {todos.map((t) => (
+      <li>{t.todo}</li>
+     ))} 
     </div>
   );
 }
