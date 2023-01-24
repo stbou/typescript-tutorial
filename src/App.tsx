209741d7ac4f079257ef0a66882d5f1412225ca4
Stephane Boulanger <stephboul22@gmail.com>
2023-01-23 22:02:27 -0500

    icon task

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
 import { Todo } from './model';
+import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
 
@@ -20,12 +21,10 @@ const App: React.FC = () => {
   console.log(todos)
 
   return (
-    <div className="App">
+    <div className="App">    
      <span className="heading">Taskify</span>
      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-     {todos.map((t) => (
-      <li>{t.todo}</li>
-     ))} 
+     <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Todo } from '../model'; 
+import { AiFillDelete, AiFillEdit  } from 'react-icons/ai'
+import { MdDone } from 'react-icons/md'
+import "./styles.css"
+
+type Props ={
+    todo: Todo;
+    todos: Todo[];
+    setTodos:  React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+
+const SingleTodo = ({todo, todos, setTodos}:Props) => {
+  return (
+    <form className='todos__single'>
+      <span className='todos__single--text'>
+        { todo.todo }
+        <div>
+            <span className='icon'>
+               <AiFillEdit />  
+            </span>
+            <span className='icon'>
+                <AiFillDelete />
+            </span>
+            <span className='icon'>
+                <MdDone /> 
+            </span>
+        </div>
+      </span>
+    </form>
+  )
+}
+
+export default SingleTodo
+ 
